a42595b1f5e5ddf135c1beb981030da7cfe87d8e
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-12-12 02:02:14 +0530

    linkin bw to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from './navbar'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Hii Im Mridul Sharma</h1>
     </div>
     // <div className={styles.container}>
---
+++ pages/navbar.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Navbar = () =>{
+    return(
+        <>
+        <div>
+            <h1>Navbar</h1>
+            <Link href="/about"> About</Link>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
