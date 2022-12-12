23e4777eb245ac74feab03d5bb845d6f50296a4f
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-12-12 13:38:01 +0530

    we cant use anchor tag inside link tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from './layout';
+import Navbar from './navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+     <Layout>
   <Component {...pageProps} />
   </Layout>
   );
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./navbar";
 export default function Layout( { children}) {
     return (
         <>
-        <Navbar />
+       <Navbar />
         {children}
         <Footer />
 
